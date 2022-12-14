de37ebe5d289c393db28b901343e7ba5531ba8e0
Oyatullo <korabeov.o@gmail.com>
2022-12-14 21:15:10 +0500

    Project Start

---
+++ PortfoliHomeWork/PortfoliHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PortfoliHomeWork
+//
+//  Created by Oyatullo Koraboev on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
